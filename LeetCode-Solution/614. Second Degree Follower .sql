8d54e19e5df6b414ae8e97c7ed33abeddbdb9d68
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-08-20 10:04:06 +0530

    Create 614. Second Degree Follower .sql

---
+++ LeetCode-Solution/614. Second Degree Follower .sql
@@ -0,0 +1,39 @@
+/*
+In facebook, there is a follow table with two columns: followee, follower.
+
+Please write a sql query to get the amount of each follower’s follower if he/she has one.
+
+For example:
+
++-------------+------------+
+| followee    | follower   |
++-------------+------------+
+|     A       |     B      |
+|     B       |     C      |
+|     B       |     D      |
+|     D       |     E      |
++-------------+------------+
+should output:
++-------------+------------+
+| follower    | num        |
++-------------+------------+
+|     B       |  2         |
+|     D       |  1         |
++-------------+------------+
+Explaination:
+Both B and D exist in the follower list, when as a followee, B's follower is C and D, and D's follower is E. A does not exist in follower list.
+ 
+
+ 
+
+Note:
+Followee would not follow himself/herself in all cases.
+Please display the result in follower's alphabet order.
+*/
+
+SELECT F1.follower , COUNT(DISTINCT(F2.follower)) AS num FROM 
+follow  F1 INNER JOIN follow F2
+ON F2.followee  = F1.follower 
+GROUP BY F1.follower
+HAVING COUNT(F2.follower)>=1
+ORDER BY  F1.follower;
